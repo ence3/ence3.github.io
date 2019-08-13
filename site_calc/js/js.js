48dd848b70a18faa36b62f70a11b58c4a77090a6
Nikita Shabanov <36622477+ence3@users.noreply.github.com>
2019-08-13 15:25:43 +0500

    Add files via upload

---
+++ site_calc/js/js.js
@@ -0,0 +1,34 @@
+let btns = document.querySelectorAll('.btn'), //Добавляем кнопки
+    screen = document.querySelector('.screen'), //Добавляем экран
+    equelBtn = document.querySelector('.btn-equel'), //Кнопка равенства
+    backBtn = document.querySelector('.btn-back'), //Кнопка удаление символа
+    clearBtn = document.querySelector('.btn-clear'); //Очистка
+
+
+    for (let i = 0; i < btns.length; i++) {  //Перебираем все кнопки в массиве
+        btns[i].addEventListener('click', function(){  //При нажатие на кнопку она добавляется на экран screen
+            let number = btns[i].getAttribute('data-num'); //Считывает данные кнопки из data-num в разметке html
+            screen.value += number; //Выводит на экран
+        });
+    };
+
+    equelBtn.addEventListener('click', function(){
+        if (screen.value == 0) { //Защита от пустой строки
+            alert('Нет чисел');
+        } else {
+        let value = eval(screen.value); //Проводится арифметические действия
+        screen.value = value.toFixed(3); //Отображается на поле результат
+        };
+    });
+
+
+    clearBtn.addEventListener('click', function(){ //Очистка поля
+        screen.value = '';
+    });
+
+
+    backBtn.addEventListener('click', function(){ //Удаление символа
+        let exp = screen.value; //Считываем поле screen
+        screen.value = exp.substring(0,  exp.length-1);  //От того, что считали отнимается последний символ и снова перезаписывается в поле
+    })
+

583bbdb3c226ae0c588dc84aef07fb58b963c226
Nikita Shabanov <36622477+ence3@users.noreply.github.com>
2023-07-15 20:36:30 +0300

    Update js.js

--- site_8/js/js.js
+++ site_8/js/js.js
@@ -17,6 +17,7 @@ let btns = document.querySelectorAll('.btn'), //Добавляем кнопки
             alert('Нет чисел');
         } else {
         let value = eval(screen.value); //Проводится арифметические действия
+            screen.value = value;
         };
     });
 
